3e41d3a0bfa516e0f3516a82ae66b4ebb564fecd
Jake Leuthardt <jakeleuthardt1@gmail.com>
2025-06-29 16:21:38 -0500

    forgot to update options

--- src/analyzers/index.ts
+++ src/analyzers/index.ts
@@ -8,9 +8,11 @@ export type AnalyzerFactory = (dir: string, emit: (msg: string) => void ) => Ana
 import { createBasicAnalyzer } from './basic.js'
 import { codeAnalyzer } from './code.js'
 import { configAnalyzer } from './config.js'
+import { docsAnalyzer } from './docs.js'
 
 export const analyzers: Record<string, AnalyzerFactory> = {
     basic: createBasicAnalyzer,
     code: codeAnalyzer,
     config: configAnalyzer,
+    docs: docsAnalyzer
 }
